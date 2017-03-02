10e90a4a84227b39a05182e454714f4efae83f9b
Ross <rossw1124@gmail.com>
2017-03-02 14:44:00 -0600

    Change some asset comilation settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_files = true
-  config.assets.compile = true
+
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -25,7 +24,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
